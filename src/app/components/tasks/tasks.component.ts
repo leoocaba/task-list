2c6bffb691d0751946f463b9bbe52e13f757543e
leoocaba <leoo.caballero@gmail.com>
2022-05-16 22:09:46 -0300

    Se realizó el delete de las tareas y el recordatorio.

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -17,10 +17,25 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     // El método suscribe() viene a funcionar como promise o async await para lograr el asincronismo.
 
-    // Dejé en el minuto 59 (09/05/2022)
     this.taskService.getTask().subscribe((tasks)=>(
       this.tasks = tasks
       ));
   }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(()=>(
+      this.tasks = this.tasks.filter( (t) => t.id !== task.id)
+    ))
+  }
+
+  // Dejé en 1 h 34' 16/05/2022
+
+  toggleReminder(task: Task) {
+    // Actualizo el front
+    task.reminder = !task.reminder;
+    // Le envio el cambio al servicio para que actualice mi DB
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
 
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
+
+// Le enviamos un JSON a nuestro servidor
+const httpOptions = {
+  headers: new HttpHeaders ({
+    'Content-Type':'application/json'
+})
+}
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +22,16 @@ export class TaskService {
 
   getTask(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiURI);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiURI}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiURI}/${task.id}`;
+    // Con la variable httpOptions le estamos diciendo al backend que le enviamos un JSON
+    return this.http.put<Task>(url, task, httpOptions);
   }
 }
